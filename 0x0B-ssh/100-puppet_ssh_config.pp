7ce45dacf32376390139bdc4fa6cc35b86bfc879
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-09-24 12:18:19 -0700

    configure SSH client using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,18 +2,18 @@
 
 file_line { 'Host':
   ensure  => present,
-  path    => '~/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   line    => 'Host 54.242.177.175',
 }
 
 file_line { 'PasswordAuthentication OFF':
   ensure  => present,
-  path    => '~/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   line    => '	PasswordAuthentication no',
 }
 
 file_line { 'Private Key identity File':
   ensure  => present,
-  path    => '~/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   line    => '	IdentityFile ~/.ssh/school',
 }
