67951f2bc1f7ab692f30ff57c37858112cd55f31
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-10-23 17:01:56 -0700

    A Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID
+returns information about his/her TODO list progress
+use urllib or requests module
+accepts an integer as a parameter, which is the employee ID
+"""
+
+
+import requests
+from sys import argv
+
+
+def emp_information():
+    """
+    returns information about employee TODO list progress
+    """
+    TASK_TITLE = []
+    TOTAL_NUMBER_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = 0
+
+    emp_id = int(argv[1])
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+
+    for user in users.json():
+        if user.get('id') == emp_id:
+            EMPLOYEE_NAME = user.get('name')
+            break
+
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
+    for task in tasks.json():
+        if task.get('userId') == emp_id:
+            TOTAL_NUMBER_OF_TASKS += 1
+            if task.get('completed') is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(
+                                                          EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_NUMBER_OF_TASKS
+                                                          ))
+
+    for title in TASK_TITLE:
+        print("\t {}".format(title))
+
+
+if __name__ == '__main__':
+    emp_information()
