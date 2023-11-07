a7835b07585c067a6a1ebead210d0fa55ed3ce45
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-11-07 14:17:22 -0800

    A recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-recursive function that queries the Reddit API 
-returns a list containing the titles of all 
+recursive function that queries the Reddit API
+returns a list containing the titles of all
 hot articles for a given subreddit
 Turn OFF redirects
 If not a valid subreddit, return None.
