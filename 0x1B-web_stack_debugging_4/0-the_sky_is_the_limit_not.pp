bdfa40b3f056763af66856a7e82decdb4629c0f3
UGBERAESE Charles A <ugberaeseac@gmail.com>
2023-11-18 22:21:23 +0000

    modify maximum open files limit setting

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,5 @@
+# modify maximum open files limit in nginx configuration
+
 exec {'modify maximum open files limit setting':
   command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
